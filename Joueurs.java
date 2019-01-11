fd651e7801b5005895f49381c66266eba887eb8c
Claire-Cussonnet <44368793+Claire-Cussonnet@users.noreply.github.com>
2019-01-11 19:06:05 +0100

    Create Joueurs.java

---
+++ Joueurs.java
@@ -0,0 +1,12 @@
+public class Joueurs {
+	
+	public String nom ;
+	public String couleur ;
+	public int numero ;
+	
+	public void Joueur (int numero, String nom, String couleur) {
+		this.numero = numero ;
+		this.nom = nom ; 
+		this.couleur = couleur ;
+	}
+}
