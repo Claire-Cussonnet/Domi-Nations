c7f4d491137d400f8e04513faddc8af4a63b060d
loudct <45229675+loudct@users.noreply.github.com>
2019-01-11 20:16:22 +0100

    Update Joueurs.java

--- Joueurs.java
+++ Joueurs.java
@@ -1,12 +1,19 @@
-public class Joueurs {
+import java.util.ArrayList;
+
+public class Joueur {
 	
 	public String nom ;
 	public String couleur ;
 	public int numero ;
 	
-	public void Joueur (int numero, String nom, String couleur) {
+	public Joueur(int numero, String nom, String couleur) {
 		this.numero = numero ;
 		this.nom = nom ; 
 		this.couleur = couleur ;
+		
+	}
+	
+	public void addPlayer( ) {
+		
 	}
 }
