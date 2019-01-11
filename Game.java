f1d8cff4aa211b00a83609839fdb62ca191418a8
Claire-Cussonnet <44368793+Claire-Cussonnet@users.noreply.github.com>
2019-01-11 19:46:14 +0100

    Update Game.java

--- Game.java
+++ Game.java
@@ -5,9 +5,38 @@ public class Game {
 	public static void main(String[] args) {
 		System.out.println("Voulez-vous jouer à 2,3 ou 4 joueurs ?"); //On demande à combien de joueurs ils souhaitent jouer
 		Scanner scan = new Scanner(System.in);
-		// try catch à faire
 		int n = scan.nextInt(); //n = nombre de joueurs
 		int k = 0;
+		String Joueurs = scan.nextLine();
+		switch(Joueurs) {
+		case "2" : 
+			System.out.println("On va jouer avec 24 dominos");
+			k = 24;
+			break;
+		case "3" : 
+			System.out.println("Tapez le nom du joueur 3 : ");
+			String nomJoueur3 = scan.nextLine();
+			
+			System.out.println("On va jouer avec 36 dominos");
+			k = 36;
+			break;
+		case "4" : 
+			System.out.println("Tapez le nom du joueur 3 : ");
+			String nomJoueur3 = scan.nextLine();
+			
+			System.out.println("Tapez le nom du joueur 4 : ");
+			String nomJoueur4 = scan.nextLine();
+			
+			System.out.println("On va jouer avec 48 dominos");
+			k = 48;
+			break;
+		
+		default :
+			System.out.println("Le nombre saisi n'est pas valide");
+			System.out.println("Ressaisissez un nombre");
+			
+			
+		}
 		
 		// try catch à faire
 		System.out.println("Tapez le nom du joueur 1 : ");
@@ -17,32 +46,6 @@ public class Game {
 		System.out.println("Tapez le nom du joueur 2 : ");
 		String nomJoueur2 = scan.nextLine();
 		
-		if (n == 2) {
-			System.out.println("On va jouer avec 24 dominos");
-			k = 24;
-
-		}
-		if (n == 3) {
-			System.out.println("Tapez le nom du joueur 3 : ");
-			String nomJoueur3 = scan.nextLine();
-			
-			System.out.println("On va jouer avec 36 dominos");
-			k = 36;
-				
-		}
-		if (n == 4) {
-			System.out.println("Tapez le nom du joueur 3 : ");
-			String nomJoueur3 = scan.nextLine();
-			
-			System.out.println("Tapez le nom du joueur 4 : ");
-			String nomJoueur4 = scan.nextLine();
-			
-			System.out.println("On va jouer avec 48 dominos");
-			k = 48;
-			
-
-		}
-
 	}
 
 }
