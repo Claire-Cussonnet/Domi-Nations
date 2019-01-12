7d7bcba2f28583a80402e4315d079035319e17e7
Claire-Cussonnet <44368793+Claire-Cussonnet@users.noreply.github.com>
2019-01-12 01:04:24 +0100

    Update and rename Domino to Domino.java

---
+++ Domino.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Domino {
+	ArrayList<Cellule> cellules = new ArrayList<>();
+	
+	public static void addCellule(ArrayList<Cellule> cellules, Cellule cellule) {
+		cellules.add(cellule);
+		}
+	public static void createDominos() {
+		
+	}
+}
+
